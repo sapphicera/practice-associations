8f8d7d9f960bb443e257842dbecb7442c1bd8a71
Alexandra <belliraxia@gmail.com>
2022-03-29 00:32:07 -0400

    adds validations

--- app/models/meeting.rb
+++ app/models/meeting.rb
@@ -1,4 +1,16 @@
 class Meeting < ApplicationRecord
   has_many :speaker_meetings
   has_many :speakers, through: :speaker_meetings
+
+  validates :title, presence: true
+  validates :agenda, presence: true
+  validates :location, presence: true
+
+  validates :time, presence: true
+  validates_each :time do |record, attr, value|
+    if value.to_i < Time.now.to_i
+      record.errors.add(attr, "time must be in the future") 
+    end
+  end
+
 end
--- app/models/speaker.rb
+++ app/models/speaker.rb
@@ -1,4 +1,8 @@
 class Speaker < ApplicationRecord
   has_many :speaker_meetings
   has_many :meetings, through: :speaker_meetings
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true
 end
